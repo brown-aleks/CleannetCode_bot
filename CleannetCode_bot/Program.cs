74f075df0078c91a43858cf263386529955897f2
Aleksandr Brown <brown-aleks@mail.ru>
2023-01-10 20:29:51 +0400

    Добавьте файлы проекта.

---
+++ CleannetCode_bot/Program.cs
@@ -0,0 +1,71 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace CleannetCode_bot
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var botClient = new TelegramBotClient(args[0]);
+
+            using CancellationTokenSource cts = new();
+
+            // Начать получение не блокирует поток вызывающего абонента. Получение выполняется в пуле потоков.
+            ReceiverOptions receiverOptions = new()
+            {
+                AllowedUpdates = Array.Empty<UpdateType>() // получать все типы обновлений
+            };
+
+            botClient.StartReceiving(
+                updateHandler: HandleUpdateAsync,
+                pollingErrorHandler: HandlePollingErrorAsync,
+                receiverOptions: receiverOptions,
+                cancellationToken: cts.Token
+            );
+
+            var me = await botClient.GetMeAsync();
+
+            Console.WriteLine("Hey! I am CleannetCode_bot.");
+            Console.ReadKey();
+
+            // Отправить запрос на отмену, чтобы остановить бота
+            cts.Cancel();
+        }
+        static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // Обрабатывать только обновления сообщений: https://core.telegram.org/bots/api#message
+            if (update.Message is not { } message)
+                return;
+            // Обрабатывать только текстовые сообщения
+            if (message.Text is not { } messageText)
+                return;
+
+            var chatId = message.Chat.Id;
+
+            Console.WriteLine($"Received a '{messageText}' message in chat {chatId}.");
+
+            // Echo received message text
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "You said:\n" + messageText,
+                cancellationToken: cancellationToken);
+        }
+
+        static Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException
+                    => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
