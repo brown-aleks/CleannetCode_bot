658a767199b220fb0ac6afad3870dd593e112ac8
Alexander Yasinovskiy <alex2021cyberhuman@gmail.com>
2023-02-11 16:00:10 +0500

    fix: Change logger prefix format

--- CleannetCode_bot/Features/Forwards/ForwardsHandler.cs
+++ CleannetCode_bot/Features/Forwards/ForwardsHandler.cs
@@ -28,47 +28,47 @@ namespace CleannetCode_bot.Features.Forwards
             ITelegramBotClient botClient,
             CancellationToken ct)
         {
-            var logPrefix = $"message: {messageId}, chat: {fromChatId}, topic: {topicId}, handler: {nameof(ForwardsHandler)}";
+            var logPrefix = $"message: {messageId}, chat: {fromChatId}, topic: {topicId}, handler: {nameof(ForwardsHandler)};";
 
-            _logger.LogDebug("{Prefix}", logPrefix);
+            _logger.LogDebug("{Prefix:l}", logPrefix);
             if (!isTopicMessage
                 || !Options.ChatsWithRestrictedTopicsWithAllowedUsersToWrite.TryGetValue(fromChatId, out var restrictedTopics))
             {
-                _logger.LogDebug("{Prefix} chat isn't restricted", logPrefix);
+                _logger.LogDebug("{Prefix:l} chat isn't restricted", logPrefix);
                 return;
             }
 
             if (!restrictedTopics.TryGetValue(topicId, out var allowedUsersToWrite))
             {
-                _logger.LogDebug("{Prefix} chat's topic {Topic} isn't restricted", logPrefix, topicId);
+                _logger.LogDebug("{Prefix:l} chat's topic {Topic} isn't restricted", logPrefix, topicId);
                 return;
             }
 
             if (allowedUsersToWrite.Contains(senderId))
             {
-                _logger.LogDebug("{Prefix} {User} of chat's topic {Topic} is allowed to write",
+                _logger.LogDebug("{Prefix:l} {User} of chat's topic {Topic} is allowed to write",
                     logPrefix,
                     senderId,
                     topicId);
                 return;
             }
 
-            _logger.LogInformation("{Prefix} {User} of chat's topic {Topic} isn't allowed to write",
+            _logger.LogInformation("{Prefix:l} {User} of chat's topic {Topic} isn't allowed to write",
                 logPrefix,
                 senderId,
                 topicId);
             var forwardingMap = Options.ChatsWithTopicsForwardMapping[fromChatId][topicId];
-            _logger.LogInformation("{Prefix} forward message of user {User} from {Topic} to chat {TargetChat} to topic {TargetTopic}",
+            _logger.LogInformation("{Prefix:l} forward message of user {User} from {Topic} to chat {TargetChat} to topic {TargetTopic}",
                 logPrefix,
                 senderId,
                 topicId,
                 forwardingMap.ChatId,
                 forwardingMap.ThreadId);
 
-            _logger.LogInformation("{Prefix} Forwarding message", logPrefix);
+            _logger.LogInformation("{Prefix:l} Forwarding message", logPrefix);
             await botClient.ForwardMessageAsync(forwardingMap.ChatId, fromChatId, messageId, forwardingMap.ThreadId, true, true, ct);
 
-            _logger.LogInformation("{Prefix} Deleting message", logPrefix);
+            _logger.LogInformation("{Prefix:l} Deleting message", logPrefix);
             await botClient.DeleteMessageAsync(fromChatId, messageId, ct);
         }
     }
